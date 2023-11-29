d108b963b8dea2b7615a7592dd7c29e2987be2b0
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-29 15:58:56 +0000

    day05 solved

---
+++ day05.js
@@ -0,0 +1,14 @@
+// Challenge 5: 
+// Write a JavaScript exercise to get the filename extension.
+function getExtension(fileName){
+    const $breakUp=fileName.split(' ');
+    if($breakUp.length>1){
+        return $breakUp[$breakUp.length-1];
+    }else{
+        return "invalid filename..";
+    }
+}
+console.log(getExtension(speech.txt));
+console.log(getExtension(cats.jpg));
+console.log(getExtension(chapter1.pdf));
+console.log(getExtension(chromeUnfinishedDownload));
