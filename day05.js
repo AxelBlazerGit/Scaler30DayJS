1de88d43d5c4a919b9adc453e10809071065ade1
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-29 16:02:26 +0000

    fixed error day05

--- day05.js
+++ day05.js
@@ -1,14 +1,14 @@
 // Challenge 5: 
 // Write a JavaScript exercise to get the filename extension.
 function getExtension(fileName){
-    const $breakUp=fileName.split('.');
-    if($breakUp.length>1){
-        return $breakUp[$breakUp.length-1];
+    const breakUp=fileName.split('.');
+    if(breakUp.length>1){
+        return breakUp[breakUp.length-1];
     }else{
         return "invalid filename..";
     }
 }
-console.log(getExtension(speech.txt));
-console.log(getExtension(cats.jpg));
-console.log(getExtension(chapter1.pdf));
-console.log(getExtension(chromeUnfinishedDownload));
+console.log(getExtension('speech.txt'));
+console.log(getExtension('cats.jpg'));
+console.log(getExtension('chapter1.pdf'));
+console.log(getExtension('chromeUnfinishedDownload'));
