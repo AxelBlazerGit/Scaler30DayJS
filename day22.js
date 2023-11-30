14ff1e55b5f96c13a605322c9a6aeffcd178fb15
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 16:26:02 +0000

    day22 solved

--- day22.js
+++ day22.js
@@ -2,3 +2,24 @@
 // Problem Statement: Write a function to deeply compare two objects for equality.  
 // Description: Given two objects, the function should return true if they are deeply equal and false otherwise.  
 // Solution Approach: Recursively compare properties of both objects.
+function deepEqual(obj1, obj2) {
+    if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null) {
+        return obj1 === obj2;
+    }
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+    for (const key of keys1) {
+        if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+            return false;
+        }
+    }
+    return true;
+}
+const obj1 = { a: 1, b: { c: 2, d: { e: 3 } } };
+const obj2 = { a: 1, b: { c: 2, d: { e: 3 } } };
+const obj3 = { a: 1, b: { c: 2, d: { e: 4 } } };
+console.log(deepEqual(obj1, obj2)); 
+console.log(deepEqual(obj1, obj3)); 
