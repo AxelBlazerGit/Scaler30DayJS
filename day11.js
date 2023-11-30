4063c2f9ee4b5eb1006fa04ab2d5562291ddc04c
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 14:38:00 +0000

    day11 solved

--- day11.js
+++ day11.js
@@ -6,3 +6,19 @@
 // sclass : "VI",
 // rollno : 12 };
 // // Sample Output: name,sclass,rollno
+var student = {
+    name: "AxelBlazer",
+    sclass: "cse1",
+    rollno: 24};
+function listProperties(obj) {
+    var result = [];
+    for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+            result.push(property);
+        }
+    }
+    return result;
+}
+var propertiesList = listProperties(student);
+console.log(propertiesList.join(','));
+
