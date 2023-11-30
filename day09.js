885f411bf58f8c711a115eb3889312f46826c832
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 14:26:35 +0000

    day08 solved

--- day09.js
+++ day09.js
@@ -2,3 +2,17 @@
 // Write a JavaScript function to find the first not repeated character.  
 // Sample arguments : 'abacddbec'  
 // Expected output : 'e'
+function firstUnrepeatedChar(str) {
+    const charCount = {};
+    for (const char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }for (const char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+    return null;
+}
+const str = 'abacddbec';
+const result = firstUnrepeatedChar(str);
+console.log(result); 
