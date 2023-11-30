3504f7efb8f0fb79d47f12237c8ea9fc876e74b1
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 15:54:15 +0000

    day17 solved

--- day17.js
+++ day17.js
@@ -9,3 +9,8 @@
 // Use the filter() method to create a new array with all elements that pass the test implemented by the provided function.
 // Solution Approach:
 // Use the filter() method and provide a callback that checks if the number is even.
+function filterOutOdd(numbers) {
+    return numbers.filter((number) => number % 2 === 0);
+}
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(filterOutOdd(array)); // odds filtered =)
