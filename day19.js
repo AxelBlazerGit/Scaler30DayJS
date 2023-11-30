1d4ac9cfb85c842f6fb51e72e1cc88d5cc64e85f
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 15:59:22 +0000

    day19 solved

--- day19.js
+++ day19.js
@@ -9,3 +9,9 @@
 // The every() method tests whether all elements in the array pass the test implemented by the provided function.
 // Solution Approach:
 // Use the every() method and provide a callback that checks if the number is positive.
+
+function allPositive(numbers) {
+    return numbers.every((number) => number > 0);
+}
+console.log(allPositive([1,2,3,4,5]));
+console.log(allPositive([1, 2, 0, 4, 5]));
