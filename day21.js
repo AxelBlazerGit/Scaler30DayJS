a84abe0cf0a8a07873a075ed40bc87a050af1c00
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 16:22:53 +0000

    day21 solved

--- day21.js
+++ day21.js
@@ -1,4 +1,25 @@
 // *Challenge 21*  
-// Problem Statement: Write an asynchronous loop function that iterates over an array and performs an asynchronous operation on each element.  
-// Description: The function should take an array and an async function, applying the async function to each element in the array in sequence.  
+// Problem Statement: Write an asynchronous loop function that iterates over an array and performs an asynchronous operation on each ele.  
+// Description: The function should take an array and an async function, applying the async function to each ele in the array in sequence.  
 // Solution Approach: Use async/await within a for...of loop.
+async function asyncLoop(array, asyncFunc) {
+    for (const ele of array) {
+        await asyncFunc(ele);
+    }
+}
+async function AsyncFunc(ele) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Processed ele: ${ele}`);
+            resolve();
+        }, 1000); 
+    });
+}
+const myArray = [1, 2, 3, 4, 5];
+asyncLoop(myArray, AsyncFunc)
+    .then(() => {
+        console.log('Async loop completed.');
+    })
+    .catch((error) => {
+        console.error('Error in async loop:', error);
+    });
