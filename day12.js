cb5c02337fd31e64f3bdbfb04039464ba94b49e5
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 15:29:50 +0000

    day12 solved

--- day12.js
+++ day12.js
@@ -8,3 +8,14 @@
 // "14:37:45"
 // "14:37:46"
 // "14:37:47"
+function displayClock() {
+    var currentDate = new Date();
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    console.log(hours + ":" + minutes + ":" + seconds);
+}
+setInterval(displayClock, 1000);
