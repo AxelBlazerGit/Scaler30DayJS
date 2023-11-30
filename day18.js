368db33d13ce0b44a547fd79a0dbd0cb9aeb0dc2
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 15:56:29 +0000

    day18 solved

--- day18.js
+++ day18.js
@@ -7,3 +7,8 @@
 // An array of numbers where each number is doubled.  
 // Hints:  
 // The map() method creates a new array with the results of calling a function for every array element.
+function doubleValues(numbers) {
+    return numbers.map((number) => number * 2);
+}
+const array = [1, 2, 3, 4, 5];
+console.log(doubleValues(array)); 
