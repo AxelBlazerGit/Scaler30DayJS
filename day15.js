d4e8307ee0a5f6344d580eb0d841c66d35cc20cc
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 15:47:51 +0000

    day15 solved

--- day15.js
+++ day15.js
@@ -6,3 +6,15 @@
 // console.log(string_chop('Scaler',2));
 // ["Scaler"]
 // ['Sc' , 'al' ,'er']
+function string_chop(str, size = 1) {
+    if (size <= 0) {
+        throw new Error('Chop factor (size) must be greater than 0');
+    }
+    const result = [];
+    for (let i = 0; i < str.length; i += size) {
+        result.push(str.slice(i, i + size));
+    }
+    return result;
+}
+console.log(string_chop('Scaler'));    
+console.log(string_chop('Scaler', 2));
