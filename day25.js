8f477ffc62848129c056495b33f75f0ddf501b3c
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 18:11:06 +0000

    day25 solved

--- day25.js
+++ day25.js
@@ -4,3 +4,15 @@
 // Use any random API that you want
 // Solution Approach:
 // Utilize the .then method to chain promises.
+const getOneData = async (url) =>
+    new Promise((res) =>
+        fetch(url).then(async response => 
+            res(await response.json())))
+
+const getData = async () => {
+    const firstData = await getOneData('https://catfact.ninja/fact');
+    const secondData = await getOneData('https://www.boredapi.com/api/activity');
+    return { ...firstData, ...secondData };
+};
+
+getData().then(finalRes => console.log(finalRes));
