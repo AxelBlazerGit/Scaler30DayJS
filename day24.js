ba6ca9b338466c699e2e3c2980efc60e9e4aa915
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 16:32:05 +0000

    day24 solved

--- day24.js
+++ day24.js
@@ -3,3 +3,20 @@
 // Implement a debounce function that takes a function and a delay as arguments. The returned function should ensure that the original function is not called more than once in the specified delay.
 // Solution Approach:
 // Use a timer and clear it on each invocation to delay the function execution.
+function debounce(func, delay) {
+    let timeoutId;
+
+    return function (...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+function handleInput(value) {
+    console.log(`Input value: ${value}`);
+}
+const debouncedHandleInput = debounce(handleInput, 1000);
+debouncedHandleInput('A');
+debouncedHandleInput('B');
+debouncedHandleInput('C');
