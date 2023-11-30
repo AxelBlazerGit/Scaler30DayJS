a896b201db2740f9f13feeace70e36c85efa95ef
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 16:28:57 +0000

    day23 solved

--- day23.js
+++ day23.js
@@ -4,3 +4,14 @@
 // Write a function counter that returns a new function on each invocation. The returned function, when called, should increment the counter value and return it.
 // Solution Approach:
 // Utilize closure to maintain the state of the counter.
+function counter() {
+    let count = 0;
+    return function () {
+        count++;
+        return count;
+    };
+}
+const incrementCounter = counter();
+console.log(incrementCounter()); // Output: 1
+console.log(incrementCounter()); // Output: 2
+console.log(incrementCounter()); // Output: 3
