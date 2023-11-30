2d28d77f898d99e1a3eed14c9549caec7bb9112f
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2023-11-30 11:12:26 +0000

    ay07 solved

--- day07.js
+++ day07.js
@@ -1,2 +1,11 @@
 // *Challenge 7*  
 // Write a JavaScript program to check a pair of numbers and return true if one of the numbers is 50 or if their sum is 50.
+function isValid(num1, num2){
+    return ((num1 === 50 || num2 === 50) || ((num1 + num2) === 50));
+}
+
+console.log(isValid(60,40));
+console.log(isValid(0,50));
+console.log(isValid(60,20));
+console.log(isValid(10,10));
+console.log(isValid(60,60));
